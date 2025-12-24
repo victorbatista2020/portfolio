25eb1fd0e6b0585bb6d1e7cfe7ac369665b59707
victorbatista2020 <victorbatista2018@hotmail.com>
2025-12-24 16:29:31 -0300

    Update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import About from '@/components/About.vue'
 import Address from '@/components/Address.vue'
 import Contact from '@/components/Contact.vue'
 import Skills from '@/components/Skills.vue'
 
+const routes = [
+  { path: '/', name: 'About', component: About },
+  { path: '/address', name: 'Address', component: Address },
+  { path: '/contact', name: 'Contact', component: Contact },
+  { path: '/skills', name: 'Skills', component: Skills },
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'About',      
-      component: About
-    },
-    {
-      path: '/',
-      name: 'Address',      
-      component: Address
-    },
-    {
-      path: '/',
-      name: 'Contact',      
-      component: Contact
-    },
-    {
-      path: '/',
-      name: 'Skills',      
-      component: Skills
-    },
-  
-  ]
+  history: createWebHashHistory(), // <--- HASH HISTORY funciona direto no GitHub Pages
+  routes
 })
 
 export default router
